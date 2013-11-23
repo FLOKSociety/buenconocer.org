b1a977430c41c51cacbf74f50bde0789e8903449
Antonio Pardo <apardo@alabs.org>
2013-11-23 10:14:28 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Buenconocer::Application.config.secret_key_base = '025497558a0c2b85efbc286d296b98fb19cb06f03d6b2d51e7ef38b9dbfd3a4e519260c3669b4ef5d5e51e7084270c8e9606a8f11782c1fdda97aed9f4c22f63'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Buenconocer::Application.config.session_store :cookie_store, key: '_buenconocer_session'
